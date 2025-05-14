dd01449299062ad90db15bafc73c7a01fb1b548b
leonardsiar <leonard.siar@estl.sg>
2025-05-14 03:12:19 +0000

    Weather page

--- server.js
+++ server.js
@@ -96,5 +96,29 @@ app.post("/delete/:id", async (req, res) => {
     }
   });
 
+  // Demo Page
+app.get('/demo', function(req, res) {
+    res.render('pages/demo');
+});  
+
+...
+// add this snippet after "var express = require('express')"
+var axios = require('axios');
+...
+
+
+// add this snippet before 
+app.get('/weather', async (req, res) => {
+    try {
+      const response = await axios.get('https://api-open.data.gov.sg/v2/real-time/api/twenty-four-hr-forecast');
+      res.render('pages/weather', { weather: response.data });
+    } catch (error) {
+      console.error(error);
+      res.send('Error fetching weather data');
+    }
+  });
+  }
+});
+
 // Tells the app which port to run on
 app.listen(8080);
